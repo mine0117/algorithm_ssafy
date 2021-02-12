bf98cccc101b41656398784087752c40278f0bc3
mine0117 <eagleeye0117@naver.com>
2021-02-12 19:18:44 +0900

    21-02-12

---
+++ D3/src/비밀번호.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class 비밀번호 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int N = sc.nextInt();
+			
+		}
+	}
+
+}
